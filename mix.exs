0bf593059d1eb9761c0e6434f07f964c7e6d00e1
ArtieFuzzz <hey@astolfo.cafe>
2025-08-02 21:42:37 +1000

    Install oban and igniter

--- mix.exs
+++ mix.exs
@@ -56,7 +56,9 @@ defmodule Vt.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:req, "~> 0.5.0"}
+      {:req, "~> 0.5.0"},
+      {:oban, "~> 2.19"},
+      {:igniter, "~> 0.5", only: [:dev]}
     ]
   end
 
